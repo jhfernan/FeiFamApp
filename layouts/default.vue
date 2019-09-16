c3d1d18d79e5f78251391b296c30fb3c049c3641
Jonathan Fernandes <jhfernan@mail.usf.edu>
2019-09-16 13:07:11 +0800

    Minor change

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 	<v-app>
 		<v-app-bar app dense dark hide-on-scroll>
 			<v-toolbar-title>
-				<nuxt-link to="/">Fei Family</nuxt-link>
+				<nuxt-link class="white--text" to="/">Fei Family</nuxt-link>
 			</v-toolbar-title>
 			<div class="flex-grow-1"></div>
 			<v-btn icon>
