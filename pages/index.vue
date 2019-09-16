a364190a3dcb406eb22fe19d89703d6abc96a956
Jonathan Fernandes <jhfernan@mail.usf.edu>
2019-09-16 10:26:22 +0800

    Updated homepage card

--- pages/index.vue
+++ pages/index.vue
@@ -4,34 +4,19 @@
 			<v-flex xs12 sm8 md6>
 				<v-card>
 					<v-card-title class="headline">
-						Welcome to the Vuetify + Nuxt.js template
+						Welome to the Fei Family App about our move to China!
 					</v-card-title>
 					<v-card-text>
-						<p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-						<p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
-						<p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+						<p>When my wife and I got married, we decided we wanted to move overseas for a couple years and teach. Little did we know that we would bring some "little helpers" along the way and we definitely did not think we would be moving to China. Nonetheless, here we are!</p>
+						<p>We decided to make an app to chronicle all our little misadventures and experiences in hopes that it might help an expat get something as simple as a meal when they finally take the plunge into this amazing place!</p>
 						<div class="text-xs-right">
-							<em><small>&mdash; John Leider</small></em>
+							<em><small>&mdash; Jonathan Fernandes</small></em>
 						</div>
-						<hr class="my-3">
-						<a
-							href="https://nuxtjs.org/"
-							target="_blank"
-						>
-							Nuxt Documentation
-						</a>
-						<br>
-						<a
-							href="https://github.com/nuxt/nuxt.js"
-							target="_blank"
-						>
-							Nuxt GitHub
-						</a>
 					</v-card-text>
 					<v-card-actions>
 						<v-spacer />
 						<v-btn color="primary" nuxt to="/tools/budget">
-							Continue
+							Check out our budget tool
 						</v-btn>
 					</v-card-actions>
 				</v-card>
